14d422ecc8423cac14d58e2de85bae35e879dcaa
bjx2130 <308110158@qq.com>
2018-11-04 20:15:16 +0800

    Update mvn_编译_启动.bat

--- spring-cloud-monitor_Version_1.5.7/mvn_编译_启动.bat
+++ spring-cloud-monitor_Version_1.5.7/mvn_编译_启动.bat
@@ -6,6 +6,13 @@
 #mvn package
 #运行
 #mvn spring-boot:run
+#
+#
+#以下3个框架版本需要兼容才可以
+#		 spring boot  		1.5.0.RELEASE
+#		 spring cloud		Edgware.SR5
+#		 spring admin		1.5.7
+#
 
 rem 合并命令
 mvn compile spring-boot:run
